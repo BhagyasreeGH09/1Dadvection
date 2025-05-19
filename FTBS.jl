96db7188047319c639b6abb5a12fb5ab5f82ad2e
BhagyasreeGH09 <bhagyasreegh21@iisertvm.ac.in>
2025-05-19 10:43:37 +0530

    "added L1,L2, and  L infinity errors"

--- FTBS.jl
+++ FTBS.jl
@@ -1,12 +1,15 @@
 using Plots
 
 L=1
-T=2
+T=1
 c=1
 
-errors = Float64[]
+L1_errors=Float64[]
+L2_errors=Float64[]
+Linf_errors=Float64[]
 deltas = Float64[]
 solutions = []
+
 
 for dx in [0.01,0.005,0.0025,0.002,0.001]
 
@@ -25,18 +28,32 @@ dt=0.5*dx/c
     end
     u[1]=u[N]
  end 
+
  u_exact = [sin(2π * (xi - c*T)) for xi in x]
- error = sqrt(sum((u .- u_exact).^2) * dx)
- push!(errors, error)
- push!(deltas, dx)
- push!(solutions, (x, copy(u)))
+
+ error = [abs(u_exact[i] - u[i]) for i in 1:N]
+  push!(L1_errors, dx * sum(error))
+  push!(L2_errors, sqrt(dx * sum(e^2 for e in error)))
+  push!(Linf_errors, maximum(error))
+  push!(deltas, dx)
+  push!(solutions, (x, copy(u)))
 end
 
-println("Order of convergence:")
-for i in 1:length(errors)-1
-    p = log2(errors[i] / errors[i+1])/log2(deltas[i] / deltas[i+1])
-    println(p)
+L1_cgs=Float64[]
+L2_cgs=Float64[]
+Linf_cgs=Float64[]
+
+for i in 1: length(deltas)-1
+     push!(L1_cgs, log2(L1_errors[i] / L1_errors[i+1]) / log2(deltas[i] / deltas[i+1]))
+    push!(L2_cgs, log2(L2_errors[i] / L2_errors[i+1]) / log2(deltas[i] / deltas[i+1]))
+    push!(Linf_cgs, log2(Linf_errors[i] / Linf_errors[i+1]) / log2(deltas[i] / deltas[i+1]))
 end
+ println(L1_errors)
+ println(L2_errors)
+ println(Linf_errors)
+ println(L1_cgs)
+ println(L2_cgs)
+ println(Linf_cgs)
 
 plot(title="Solutions at final time T=$T", xlabel="x", ylabel="u(x,T)", legend=:bottomleft)
 
--- Lax-Wendroff.jl
+++ Lax-Wendroff.jl
@@ -1,10 +1,12 @@
 using Plots
 
 L=1
-T=2
+T=1
 c=1
 
-errors = Float64[]
+L1_errors=Float64[]
+L2_errors=Float64[]
+Linf_errors=Float64[]
 deltas = Float64[]
 solutions = []
 
@@ -28,18 +30,31 @@ v=c*(dt/dx)
     u[N]= uold[N] - v*(uold[1] - uold[N-1])/2 + (v^2)*(uold[1] - 2*uold[N] + uold[N-1])/2
     u[1] = uold[1] -v*(uold[2] - uold[N]) + (v^2)*(uold[2] - 2*uold[1] + uold[N])/2
  end 
- u_exact = [sin(2π * (xi - c*T)) for xi in x]
- error = sqrt(sum((u .- u_exact).^2) * dx)
- push!(errors, error)
- push!(deltas, dx)
- push!(solutions, (x, copy(u)))
-end
 
-println("Order of convergence:")
-for i in 1:length(errors)-1
-    p = log2(errors[i] / errors[i+1])/log2(deltas[i] / deltas[i+1])
-    println(p)
+  u_exact = [sin(2π * (xi - c*T)) for xi in x]
+
+  error = [abs(u_exact[i] - u[i]) for i in 1:N]
+  push!(L1_errors, dx * sum(error))
+  push!(L2_errors, sqrt(dx * sum(e^2 for e in error)))
+  push!(Linf_errors, maximum(error))
+  push!(deltas, dx)
+  push!(solutions, (x, copy(u)))
 end
+L1_cgs=Float64[]
+L2_cgs=Float64[]
+Linf_cgs=Float64[]
+
+for i in 1: length(deltas)-1
+     push!(L1_cgs, log2(L1_errors[i] / L1_errors[i+1]) / log2(deltas[i] / deltas[i+1]))
+    push!(L2_cgs, log2(L2_errors[i] / L2_errors[i+1]) / log2(deltas[i] / deltas[i+1]))
+    push!(Linf_cgs, log2(Linf_errors[i] / Linf_errors[i+1]) / log2(deltas[i] / deltas[i+1]))
+end
+ println(L1_errors)
+ println(L2_errors)
+ println(Linf_errors)
+ println(L1_cgs)
+ println(L2_cgs)
+ println(Linf_cgs)
 
 plot(title="Solutions at final time T=$T", xlabel="x", ylabel="u(x,T)", legend=:bottomleft)
 
